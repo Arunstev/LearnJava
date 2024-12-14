d3dea0bf358f607c0764aaa47a5870e3c9e51f8b
Arunstev <121638809+Arunstev@users.noreply.github.com>
2024-12-14 12:46:15 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Helloworld!");
-    }
+import edu.duke.*;
+
+public class HelloWorld {
+	public void runHello () {
+		FileResource res = new FileResource("hello_unicode.txt");
+		for (String line : res.lines()) {
+			System.out.println(line);
+		}
+	}
 }
